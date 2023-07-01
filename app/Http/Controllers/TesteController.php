87fc4b0554bcffb1db9eaeef7af7b834ebf6e774
Henrique <hlopessantos52@gmail.com>
2023-07-01 10:21:39 -0300

    passando parametros para controllers

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -8,7 +8,10 @@ class TesteController extends Controller
 {
     public function teste($p1, $p2)
     {
-        echo "a soma de $p1 + $p2 é: ".($p1+$p2);
+//        echo "a soma de $p1 + $p2 é: ".($p1+$p2);
+//        return view('site.teste', ['x'=> $p1, 'y'=> $p2]); // array associativo
+//        return view('site.teste', compact('p1', 'p2')); // compact('p1', 'p2') é o mesmo que ['p1'=> $p1, 'p2'=> $p2]
+        return view('site.teste')->with('xyz',$p1)->with('abc',$p2); // with('nome da variavel', 'valor da variavel'
     }
 
 }
