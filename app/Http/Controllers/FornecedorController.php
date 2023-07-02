582944e0035cfae22f1d81a3a595ed49cd0d42e3
Henrique <hlopessantos52@gmail.com>
2023-07-02 11:29:12 -0300

    add loop in index.blade.php

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,6 +8,30 @@ class FornecedorController extends Controller
 {
     public function index()
     {
-        return view('app.fornecedor.index');
+        $fornecedores = [
+            0 => [
+                'nome' => 'fornecedor 1',
+                'status' => 'N',
+//                'cnpj' => '00.000.000/0000-00',
+                'cnpj' => '',
+                'ddd' => '11',
+                'telefone' => '0000-0000'
+            ],
+            1 => [
+                'nome' => 'fornecedor 2',
+                'status' => 'S',
+                'cnpj' => '00.000.000/0000-00',
+                'ddd' => '22',
+                'telefone' => '0000-0000'
+            ],
+            2 => [
+                'nome' => 'fornecedor 3',
+                'status' => 'S',
+                'cnpj' => '00.000.000/0000-00',
+                'ddd' => '33',
+                'telefone' => '0000-0000'
+            ],
+        ];
+        return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
