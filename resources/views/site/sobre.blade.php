b158ac92502b3ba181df5858d26ffae08ee4c99f
Henrique <hlopessantos52@gmail.com>
2023-07-02 16:52:48 -0300

    Adding new file and refactoring views

--- resources/views/site/sobre.blade.php
+++ resources/views/site/sobre.blade.php
@@ -1,20 +1,8 @@
 @extends('site.layouts.basico')
 
+@section('titulo', 'Sobre')
+
 @section('conteudo')
-    <div class="topo">
-
-        <div class="logo">
-            <img src="img/logo.png">
-        </div>
-
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('site.index') }}">Principal</a></li>
-                <li><a href="{{ route('site.sobre') }}">Sobre</a></li>
-                <li><a href="{{ route('site.contato') }}">Contato</a></li>
-            </ul>
-        </div>
-    </div>
 
     <div class="conteudo-pagina">
         <div class="titulo-pagina">
