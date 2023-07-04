7f485d65073d48e64f43e5f637f3ddafa13c66bb
Henrique <hlopessantos52@gmail.com>
2023-07-03 21:51:19 -0300

    add migration ajuste produtos e filiais tables

---
+++ database/migrations/2023_07_04_003639_ajuste_produtos_filiais.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('filiais', function (Blueprint $table){
+            $table->id();
+            $table->string('filial', 30);
+            $table->timestamps();
+        });
+
+        Schema::create('produto_filiais', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('produto_id');
+            $table->unsignedBigInteger('filial_id');
+            $table->float('preco_venda', 8, 2)->default(0.01);
+            $table->integer('estoque_minimo')->default(1);
+            $table->integer('estoque_maximo')->default(1);
+            $table->timestamps();
+
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            $table->foreign('filial_id')->references('id')->on('filiais');
+        });
+
+        // Removendo colunas da tabela produtos
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->dropColumn(['preco_venda', 'estoque_minimo', 'estoque_maximo']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Adicionando colunas na tabela produtos
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->float('preco_venda', 8, 2)->default(0.01);
+            $table->integer('estoque_minimo')->default(1);
+            $table->integer('estoque_maximo')->default(1);
+        });
+
+        Schema::dropIfExists('produto_filiais');
+        Schema::dropIfExists('filiais');
+    }
+};
