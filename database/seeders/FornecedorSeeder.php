ec2d82dd3696c2defaa1a022eb7323a1e31ea190
Henrique <hlopessantos52@gmail.com>
2023-07-04 07:55:01 -0300

    create seed FornecedorSeeder.php

---
+++ database/seeders/FornecedorSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Fornecedor;
+
+class FornecedorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'fornecedor100.com.br';
+        $fornecedor->uf = 'SP';
+        $fornecedor->email = 'fornecedor100@contato.com.br';
+        $fornecedor->save();
+    }
+}
