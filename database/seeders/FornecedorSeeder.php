845530c1804ff1fd88bd16067a7f1355c856e1c4
Henrique <hlopessantos52@gmail.com>
2023-07-04 08:02:37 -0300

    finishing seeder FornecedorSeeder.php

--- database/seeders/FornecedorSeeder.php
+++ database/seeders/FornecedorSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Fornecedor;
+use Illuminate\Support\Facades\DB;
 
 class FornecedorSeeder extends Seeder
 {
@@ -13,11 +14,30 @@ class FornecedorSeeder extends Seeder
      */
     public function run(): void
     {
+        // instanciando o objeto
         $fornecedor = new Fornecedor();
         $fornecedor->nome = 'Fornecedor 100';
         $fornecedor->site = 'fornecedor100.com.br';
         $fornecedor->uf = 'SP';
         $fornecedor->email = 'fornecedor100@contato.com.br';
         $fornecedor->save();
+
+        // usando o método create (atenção para o atributo fillable da classe)
+        Fornecedor::create([
+            'nome' => 'Fornecedor 200',
+            'site' => 'fornecedor200.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@fornecedor200.com.br'
+        ]);
+
+        // insert (nao passa pelo eloquente
+        DB::table('fornecedores')->insert([
+            'nome' => 'Fornecedor 300',
+            'site' => 'fornecedor300.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@fornecedor300.com.br'
+        ]);
     }
+
+
 }
