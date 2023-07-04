d76ffdf7a9a21b85389b6577e235a4ead95eeb82
Henrique <hlopessantos52@gmail.com>
2023-07-04 07:54:52 -0300

    edit Fornecedor.php

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -9,4 +9,5 @@ class Fornecedor extends Model
 {
 //    use HasFactory;
     protected $table = 'fornecedores'; // usado para quando o nome da tabela não é o plural do nome do model
+    protected $fillable = ['nome', 'site', 'uf', 'email']; // usado para quando o nome da tabela não é o plural do nome do model
 }
