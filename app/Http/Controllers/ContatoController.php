fa0bd02cdf45524f2726d4a3a4ab381a5d2eb747
Henrique <hlopessantos52@gmail.com>
2023-07-05 09:15:01 -0300

    fix problem connect refuse db_host mysql

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
@@ -9,12 +10,17 @@ class ContatoController extends Controller
     public function contato(Request $request)
     {
 //        var_dump($_POST);
-        echo '<pre>';
-        print_r($request->all());
-        echo '</pre>';
-        echo $request->input('nome');
-        echo '<br>';
-        echo $request->input('email');
-        return view('site.contato');
+//        echo '<pre>';
+//        print_r($request->all());
+//        echo '</pre>';
+//        echo $request->input('nome');
+//        echo '<br>';
+//        echo $request->input('email');
+
+        $contato = new SiteContato();
+        $contato->create($request->all());
+        return view('site.contato', ['titulo' => 'Contato (teste)']);
+
+
     }
 }
