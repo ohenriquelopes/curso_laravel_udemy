257d02a54087058a5bdcad15548012fcc4cb9c17
Henrique <hlopessantos52@gmail.com>
2023-07-10 08:47:04 -0300

    add request_old_input

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -17,10 +17,42 @@ class ContatoController extends Controller
 //        echo '<br>';
 //        echo $request->input('email');
 
+//        $contato = new SiteContato();
+//        $contato->create($request->all());
+
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação'
+        ];
+        return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos]);
+
+
+    }
+
+    public function salvar(Request $request)
+    {
+
+        $request->validate([
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
+            'mensagem' => 'required|max:2000',
+        ], [
+            'nome.required' => 'O campo nome precisa ser preenchido',
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome precisa ter no máximo 40 caracteres',
+            'telefone.required' => 'O campo telefone precisa ser preenchido',
+            'email.email' => 'O campo email não foi preenchido corretamente',
+            'motivo_contatos_id.required' => 'O campo motivo de contato precisa ser preenchido',
+            'mensagem.required' => 'O campo mensagem precisa ser preenchido',
+            'mensagem.max' => 'O campo mensagem precisa ter no máximo 2000 caracteres',
+        ]);
         $contato = new SiteContato();
         $contato->create($request->all());
-        return view('site.contato', ['titulo' => 'Contato (teste)']);
 
 
+//        SiteContato()::create($request->all());
     }
 }
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -1,20 +1,33 @@
 {{ $slot }}
 <form action={{ route('site.contato') }} method="post" >
     @csrf
-    <input name="nome" placeholder="Nome" class="{{ $classe }}">
+    <input name="nome" value="{{ old('nome') }}" placeholder="Nome" class="{{ $classe }}">
     <br>
-    <input name="telefone" type="text" placeholder="Telefone" class="{{ $classe }}">
+    <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
     <br>
-    <input name="email" type="text" placeholder="E-mail" class="{{ $classe }}">
+    <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
     <br>
+    {{ print_r($motivo_contatos) }}
     <select name="motivo_contato" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
-        <option value="1">Dúvida</option>
-        <option value="2">Elogio</option>
-        <option value="3">Reclamação</option>
+        <option value="1" {{ old('motivo_contato') == 1 ? 'selected' : '' }} >Dúvida</option>
+        <option value="2" {{ old('motivo_contato') == 2 ? 'selected' : '' }} >Elogio</option>
+        <option value="3" {{ old('motivo_contato') == 3 ? 'selected' : '' }} >Reclamação</option>
     </select>
     <br>
-    <textarea name="mensagem" class="{{ $classe }}">Preencha aqui a sua mensagem</textarea>
+{{--    para fazer o formulario preencher novamente os campos anteriores se der erro no post--}}
+        @if(old('mensagem') != '')
+           @php $mensagem = old('mensagem'); @endphp
+        @else
+            @php $mensagem = 'Preencha aqui a sua mensagem'; @endphp
+        @endif
+    <textarea name="mensagem" class="{{ $classe }}" placeholder="Informe aqui sua mensagem">{{ $mensagem }}</textarea>
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
+
+{{--<div style="position: absolute; top:0px; left: 0px; width: 100%; "  >--}}
+{{--    <pre>--}}
+{{--        {{ print_r($errors) }}--}}
+{{--    </pre>--}}
+{{--</div>--}}
