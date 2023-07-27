55882fba4093e111e6a27f0fbc1b7ddcf7c7e615
henrique <hlopessantos52@gmail.com>
2023-07-26 23:32:06 -0300

    include multiples params to middleware

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -13,14 +13,26 @@ class AutenticacaoMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, $metodo_autenticacao, $perfil): Response
     {
+        echo $metodo_autenticacao.' - '.$perfil.'<br>';
+        if($metodo_autenticacao == 'padrao'){
+            echo 'verificando autenticacao padrao '.$perfil.'<br>';
+        } else {
+            echo 'verificando autenticacao alternativa '.$perfil.'<br>';
+        }
+
+        if($perfil == 'visitante') {
+            echo 'Exibir apenas alguns recursos do site'.'<br>';
+        }
+
         if(true){
             return $next($request);
         } else {
             return Response('Acesso negado! Rota existe autenticacao');
         }
 //        return $next($request);
+
     }
 
 }
