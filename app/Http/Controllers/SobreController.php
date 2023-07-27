aa439907ec2d7cb076ec443e0d136760fba50700
henrique <hlopessantos52@gmail.com>
2023-07-26 23:12:50 -0300

    forcando o middleware no controller passando o apelido

--- app/Http/Controllers/SobreController.php
+++ app/Http/Controllers/SobreController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Middleware\LogAcessoMiddleware;
 
 class SobreController extends Controller
 {
 
+    // faz o controller passar pelo middleware usando o apelido no arquivo kernel.php
     public function __construct()
     {
-        $this->middleware(LogAcessoMiddleware::class);
+        $this->middleware('log.acesso');
     }
+
     public function sobre()
     {
         return view('site.sobre');
